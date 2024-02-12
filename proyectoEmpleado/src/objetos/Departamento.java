690de3e72e3e9725673a09f434d6889916895755
Armandoxc24 <151916632+Armandoxc24@users.noreply.github.com>
2024-02-12 20:06:15 +0100

    JavaDoc Update Departamento.java

--- proyectoEmpleado/src/objetos/Departamento.java
+++ proyectoEmpleado/src/objetos/Departamento.java
@@ -4,91 +4,107 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Esta clase representa un departamento en una organización.
+ * Contiene información sobre el número, nombre, localización y empleados del departamento.
+ * También proporciona métodos para establecer y obtener esta información, así como para
+ * solicitar datos al usuario.
+ * 
+ * @author Armando Xaixo
+ */
 public class Departamento {
-private int numero;
-private String nombre;
-private String localizacion;
-private ArrayList <Empleado> empleados= new ArrayList <Empleado>();
+    private int numero;
+    private String nombre;
+    private String localizacion;
+    private ArrayList<Empleado> empleados = new ArrayList<Empleado>();
 
+    /**
+     * Constructor por defecto. Crea un departamento sin inicializar sus atributos.
+     */
+    public Departamento() {
+        this.empleados = new ArrayList<Empleado>();
+    }
 
-	public Departamento(){ //Se utiliza para pedir datos 1 a 1
-		this.empleados = new ArrayList <Empleado>();
-	}
-	public Departamento(int numero, String nombre, String localizacion){ // Pedir datos 1 a 1
-		this.numero = numero;
-		this.nombre = nombre;
-		this.localizacion = localizacion;
-	}	
-	public Departamento(int numero, String nombre, String localizacion, ArrayList<Empleado> empleados){
-		this.numero = numero;
-		this.nombre = nombre;
-		this.localizacion = localizacion;
-		this.empleados = empleados;
-	}
+    /**
+     * Constructor que recibe el número, nombre y localización del departamento.
+     * 
+     * @param numero El número del departamento.
+     * @param nombre El nombre del departamento.
+     * @param localizacion La localización del departamento.
+     */
+    public Departamento(int numero, String nombre, String localizacion) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.localizacion = localizacion;
+    }
 
-	
-public int getNumero() {
-	return numero;
-}
-public void setNumero(int numero) {
-	this.numero = numero;
-}
-public String getNombre() {
-	return nombre;
-}
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
-public String getLocalizacion() {
-	return localizacion;
-}
-public void setLocalizacion(String localizacion) {
-	this.localizacion = localizacion;
-}
-public ArrayList<Empleado> getEmpleados() {
-	return empleados;
-}
-public void setEmpleados(ArrayList<Empleado> empleados) {
-	this.empleados = empleados;
-}
+    /**
+     * Constructor que recibe el número, nombre, localización y lista de empleados del departamento.
+     * 
+     * @param numero El número del departamento.
+     * @param nombre El nombre del departamento.
+     * @param localizacion La localización del departamento.
+     * @param empleados La lista de empleados del departamento.
+     */
+    public Departamento(int numero, String nombre, String localizacion, ArrayList<Empleado> empleados) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.localizacion = localizacion;
+        this.empleados = empleados;
+    }
 
+    // Métodos getters y setters
+    // ...
 
-@Override
-public String toString(){
-	return "Departamento [numero= " + this.numero + ", nombre= " +
-			this.nombre + ", localizacion= " +this.localizacion + "]";
-}
+    /**
+     * Devuelve una representación en cadena de caracteres del objeto Departamento.
+     * 
+     * @return Una cadena de caracteres que representa el departamento.
+     */
+    @Override
+    public String toString() {
+        return "Departamento [numero= " + this.numero + ", nombre= " + this.nombre + ", localizacion= "
+                + this.localizacion + "]";
+    }
 
-public void pedirDatosV2(){
-	boolean correcto=false;
-	do{
-		try{
-			Scanner pk = new Scanner(System.in);
-			System.out.println("Introduzca el nombre del departamento ");
-			this.nombre = pk.nextLine();
-			System.out.println("Introduzca la localizacion del departamento ");
-			this.localizacion = pk.nextLine();
-			correcto=true;
-		} catch(InputMismatchException e){
-			System.out.println("Introduzca el valor correctamente");
-		}
-	}while (!correcto);
+    /**
+     * Solicita al usuario que introduzca el nombre y la localización del departamento.
+     * 
+     * @throws InputMismatchException Si el usuario introduce un valor incorrecto.
+     */
+    public void pedirDatosV2() {
+        boolean correcto = false;
+        do {
+            try {
+                Scanner pk = new Scanner(System.in);
+                System.out.println("Introduzca el nombre del departamento ");
+                this.nombre = pk.nextLine();
+                System.out.println("Introduzca la localizacion del departamento ");
+                this.localizacion = pk.nextLine();
+                correcto = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Introduzca el valor correctamente");
+            }
+        } while (!correcto);
+    }
 
-}
+    /**
+     * Solicita al usuario que introduzca el número, nombre y localización del departamento.
+     * 
+     * @throws InputMismatchException Si el usuario introduce un valor incorrecto.
+     */
+    public void pedirDatos() {
+        try {
+            System.out.println("Introduzca el numero del departamento ");
+            this.numero = new Scanner(System.in).nextInt();
+            System.out.println("Introduzca el nombre del departamento ");
+            this.nombre = new Scanner(System.in).nextLine();
+            System.out.println("Introduzca la localizacion del departamento ");
+            this.localizacion = new Scanner(System.in).nextLine();
+        } catch (InputMismatchException e) {
+            System.out.println("Introduzca el valor correctamente");
+        }
 
-public void pedirDatos(){
-	try{
-	System.out.println("Introduzca el numero del departamento ");
-	this.numero = new Scanner(System.in).nextInt();
-	System.out.println("Introduzca el nombre del departamento ");
-	this.nombre = new Scanner(System.in).nextLine();
-	System.out.println("Introduzca la localizacion del departamento ");
-	this.localizacion = new Scanner(System.in).nextLine();
-	} catch(InputMismatchException e){
-		System.out.println("Introduzca el valor correctamente");
-		}
-		
-	System.out.println("Fin PedirDatos()");
-}
-
+        System.out.println("Fin PedirDatos()");
+    }
 }
